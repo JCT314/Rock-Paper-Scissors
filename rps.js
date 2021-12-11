8436595854aa37b883b39f001fe1079aa50763e5
Jose Christopher Toscano <jctoscano1@gmail.com>
2021-12-11 00:24:41 -0800

    Added winning conditions and reset button

--- rps.js
+++ rps.js
@@ -29,10 +29,52 @@ function onButtonClick(e) {
     const computerSelection = computerPlay();
     const {text,winner} = playRound(playerSelection,computerSelection);
     outcomeEl.textContent = text;
-    
+    round++;
+    roundEl.textContent = `Round ${round}`;
+    if(winner === "player") {
+        playerScore++;
+    }
+    if(winner === "computer") {
+        computerScore++;
+    }
+    playerScoreEl.textContent = `Player:${playerScore}`;
+    computerScoreEl.textContent = `${computerScore}:Computer`;
+    if(playerScore === 5) {
+        outcomeEl.textContent = "You win!"
+        resetTextEl.style.opacity = 1;
+        btns.removeEventListener("click",onButtonClick);
+    }
+    if(computerScore === 5) {
+        outcomeEl.textContent = "You lost!"; 
+        resetTextEl.style.opacity = 1;
+        btns.removeEventListener("click",onButtonClick);
+    }
+}
+
+function resetGame() {
+    playerScore = 0;
+    computerScore = 0;
+    round = 1;
+    resetTextEl.style.opacity = 0;
+    outcomeEl.textContent = "Choose Your Weapon to Begin!";
+    playerScoreEl.textContent = `Player:${playerScore}`;
+    computerScoreEl.textContent = `${computerScore}:Computer`;
+    roundEl.textContent = `Round ${round}`;
+    btns.addEventListener('click', onButtonClick);
 }
 
 const btns = document.querySelector(".btns");
+const btnReset = document.querySelector(".btn--reset");
 const outcomeEl = document.querySelector(".outcome");
+const playerScoreEl = document.querySelector(".score-player");
+const computerScoreEl = document.querySelector(".score-computer");
+const resetTextEl = document.querySelector(".reset-text");
+const roundEl = document.querySelector(".round-display");
+let playerScore = 0;
+let computerScore = 0;
+let round = 1;
+
 btns.addEventListener('click', onButtonClick);
+btnReset.addEventListener('click',resetGame);
+
 
